0bee8e07b5a6bf6215966949777c4defa50fe77e
ariel434 <arielnimni218@gmail.com>
2021-10-12 22:14:25 +0300

    added gome page

--- angular/timely/src/app/app.module.ts
+++ angular/timely/src/app/app.module.ts
@@ -3,16 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [HttpClient,HttpClientModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
