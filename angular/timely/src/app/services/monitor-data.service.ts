04747e545762a0bf9a6787d7266b9b2c0ff6e9b0
ariel434 <arielnimni218@gmail.com>
2021-10-14 22:41:59 +0300

    http

--- angular/timely/src/app/services/monitor-data.service.ts
+++ angular/timely/src/app/services/monitor-data.service.ts
@@ -8,7 +8,7 @@ export class MonitorDataService {
 
   constructor(private _http:HttpClient) { }
 
-apiUrl:string="http://10.0.0.2:4200/";
+apiUrl:string="http://10.0.0.2:7750/";
 
 startMonitor(){
     return this._http.get<string>(this.apiUrl+'monitor_on');
