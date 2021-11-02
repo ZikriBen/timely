1bc481b718ef07bbb9e71a50a9d9574f7c84d6e9
ariel434 <arielnimni218@gmail.com>
2021-11-02 20:47:54 +0200

    add time calculate

--- angular/timely/src/app/components/home/home/home.component.ts
+++ angular/timely/src/app/components/home/home/home.component.ts
@@ -11,40 +11,59 @@ export class HomeComponent implements OnInit {
   constructor(private monitorDataService:MonitorDataService) { }
 public startTime:number;
 public endTime:number;
-
+public time:number;
+public noChangedMadeTime:number;
+public workTime:number;
+public isLoading:boolean=false;
+public status:string;
   ngOnInit(): void {
   }
 
   startMonitor(){
-this.monitorDataService.startMonitor().subscribe(data=>{
-        console.log(data)
-        // if (data!=null) {
+    this.time=null;
+    this.workTime=null;
+    this.noChangedMadeTime=null;
 
-        // this.startTime=performance.now()
-        // console.log(this.startTime+' im start  ')
-        // }
+
+this.monitorDataService.startMonitor().subscribe(data=>{
+if (data!=null) {
+    this.isLoading=true;
+
+}
+        this.startTime=performance.now()
+
 })
 
 
 
   }
 stopMonitor(){
+  this.endTime=performance.now();
+  this.time=(this.endTime-this.startTime);
 this.monitorDataService.stopMonitor().subscribe(data=>{
        if (data!=null){
-         console.log(JSON.stringify(data) )
+       this.noChangedMadeTime = data.reduce(function (r, a) {
+  r[0] = (r[0] || 0) + a[0];
 
-        //  this.endTime=(performance.now()-this.startTime);
-        //  this.endTime/=1000;
-        //  Math.round(this.endTime);
-        //  console.log(this.endTime+'im finishe')
+  return r;
+}, []);
+
+
+
+
+
        }
+       this.workTime=this.time-(this.noChangedMadeTime*1000);
+    this.isLoading=false;
+
 
 })
 }
 statusMonitor(){
 this.monitorDataService.statusMonitor().subscribe(data=>{
-        console.log(data)
-
+if (data!=null) {
+  this.status=data;
+}
 })
 }
 }
--- angular/timely/src/app/services/monitor-data.service.ts
+++ angular/timely/src/app/services/monitor-data.service.ts
@@ -8,16 +8,16 @@ export class MonitorDataService {
 
   constructor(private _http:HttpClient) { }
 
-apiUrl:string="http://10.0.0.5:7750/";
+apiUrl:string="http://10.0.0.2:7750/";
 
 startMonitor(){
 
-    return this._http.get(this.apiUrl+'monitor_on');
+    return this._http.get<string>(this.apiUrl+'monitor_on');
 
 
 }
 stopMonitor(){
-    return this._http.post<any>(this.apiUrl+'monitor_off',{});
+    return this._http.get<any>(this.apiUrl+'monitor_off');
 
 }
 statusMonitor(){
